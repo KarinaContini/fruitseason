6bae7091660ea0ce1505e20c714f530b4b8b074f
gabriellamiwa <gabriellakaku@gmail.com>
2024-02-29 19:29:29 -0500

    Update FruitsSale.java

--- app/src/main/java/com/example/fruitseason/FruitsSale.java
+++ app/src/main/java/com/example/fruitseason/FruitsSale.java
@@ -1,8 +1,12 @@
 package com.example.fruitseason;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +19,16 @@ public class FruitsSale extends AppCompatActivity {
         setContentView(R.layout.fruit_sale);
         listView = findViewById(R.id.listv);
         String[] fruits = {"Pineapple", "Banana", "Strawberry"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.fruits_list_sale, R.id.textFruits, fruits);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.fruits_list_sale, R.id.txtFruits, fruits);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String selectedItem = (String) adapterView.getItemAtPosition(position);
+                Intent myIntent = new Intent(FruitsSale.this, FruitInfo.class);
+                myIntent.putExtra("fruit", selectedItem);
+                startActivity(myIntent);
+            }
+        } );
     }
 }
