0a7b41e12d4aa294b389beb917d994203d45a889
gabriellamiwa <gabriellakaku@gmail.com>
2024-02-29 19:29:24 -0500

    Create FruitInfo.java

---
+++ app/src/main/java/com/example/fruitseason/FruitInfo.java
@@ -0,0 +1,21 @@
+package com.example.fruitseason;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FruitInfo extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fruit_info);
+        textView = findViewById(R.id.txtFruit);
+        Intent intent = getIntent();
+        String fruit = intent.getStringExtra("fruit");
+        textView.setText(fruit);
+
+    }
+}
