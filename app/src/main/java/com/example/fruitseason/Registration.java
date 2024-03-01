0c417a2048a8e6ffe9ba92851bafd53af4a3f7c9
gabriellamiwa <gabriellakaku@gmail.com>
2024-02-29 20:29:26 -0500

    Update Registration.java

--- app/src/main/java/com/example/fruitseason/Registration.java
+++ app/src/main/java/com/example/fruitseason/Registration.java
@@ -1,13 +1,25 @@
 package com.example.fruitseason;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Registration extends AppCompatActivity {
+    Button buttonReg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
+        buttonReg = findViewById(R.id.btnRegister);
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Registration.this, FruitsSale.class);
+                startActivity(intent);
+            }
+        });
     }
 }
