aa7445eab5903882232cfeab7db5d10937afcff8
larimas <larissamasousa@gmail.com>
2024-03-10 16:00:48 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/fruitseason/MainActivity.java
+++ app/src/main/java/com/example/fruitseason/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.fruitseason;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
