ec4d8d220eb53a1ab633a8e2d5b11e44b670d947
gabriellamiwa <gabriellakaku@gmail.com>
2024-03-19 22:18:03 -0400

    Update FruitsSale.java

--- app/src/main/java/com/example/fruitseason/FruitsSale.java
+++ app/src/main/java/com/example/fruitseason/FruitsSale.java
@@ -1,12 +1,15 @@
 package com.example.fruitseason;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,7 @@ public class FruitsSale extends AppCompatActivity {
         setContentView(R.layout.activity_fruit_sale);
         listView = findViewById(R.id.listv);
         searchFruits = findViewById(R.id.searchView);
+
         String[] fruits = {"Pineapple", "Banana", "Strawberry"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.fruits_list_sale, R.id.txtFruits, fruits);
         listView.setAdapter(adapter);
