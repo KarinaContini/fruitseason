fe54a58da15dd09d6c9cea47fa1cf1b643e32086
gabriellamiwa <gabriellakaku@gmail.com>
2024-04-01 17:54:44 -0400

    Update FruitInfo.java

--- app/src/main/java/com/example/fruitseason/FruitInfo.java
+++ app/src/main/java/com/example/fruitseason/FruitInfo.java
@@ -1,21 +1,74 @@
 package com.example.fruitseason;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class FruitInfo extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class FruitInfo extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     TextView textView;
+    private DrawerLayout drawerLayout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fruit_info);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EditProfileFragment()).commit();
+            navigationView.setCheckedItem(R.id.editProfile);
+        }
         textView = findViewById(R.id.txtFruit);
         Intent intent = getIntent();
         String fruit = intent.getStringExtra("fruit");
         textView.setText(fruit);
 
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.editProfile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EditProfileFragment()).commit();
+                break;
+            case R.id.myFruits:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyFruitsFragment()).commit();
+                break;
+            case R.id.logout:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LogoutFragment()).commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.getOnBackPressed();
+        }
     }
 }
