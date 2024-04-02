2161f1bc14ea43eac70cc6d56ea57ac7b496d3a3
gabriellamiwa <gabriellakaku@gmail.com>
2024-04-02 14:54:30 -0400

    Update FruitInfo.java

--- app/src/main/java/com/example/fruitseason/FruitInfo.java
+++ app/src/main/java/com/example/fruitseason/FruitInfo.java
@@ -1,74 +1,70 @@
 package com.example.fruitseason;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.PopupMenu;
+import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.appcompat.widget.Toolbar;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.material.navigation.NavigationView;
 
-public class FruitInfo extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class FruitInfo extends AppCompatActivity{
     TextView textView;
     private DrawerLayout drawerLayout;
+    private ImageView menu;
+    TextView editProfile;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fruit_info);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,R.string.close_nav);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EditProfileFragment()).commit();
-            navigationView.setCheckedItem(R.id.editProfile);
-        }
+
+        editProfile = findViewById(R.id.editProfile);
+        menu= findViewById(R.id.menu);
+
         textView = findViewById(R.id.txtFruit);
         Intent intent = getIntent();
         String fruit = intent.getStringExtra("fruit");
         textView.setText(fruit);
-
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDrawer(drawerLayout);
+            }
+        });
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(FruitInfo.this, PasswordChange.class);
+            }
+        });
     }
-
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.editProfile:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EditProfileFragment()).commit();
-                break;
-            case R.id.myFruits:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyFruitsFragment()).commit();
-                break;
-            case R.id.logout:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LogoutFragment()).commit();
-                break;
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
+public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+}
+public static void closeDrawer(DrawerLayout drawerLayout){
         if(drawerLayout.isDrawerOpen(GravityCompat.START)){
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else{
-            super.getOnBackPressed();
         }
     }
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity,secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+
+
 }
