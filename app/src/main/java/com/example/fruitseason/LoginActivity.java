44b4c996e88d9a3c730a73621edb6c8fb26c4eb7
KarinaContini <ka.siviero@gmail.com>
2024-04-04 04:02:22 -0400

    login igual atual

--- app/src/main/java/com/example/fruitseason/LoginActivity.java
+++ app/src/main/java/com/example/fruitseason/LoginActivity.java
@@ -10,28 +10,78 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
 
-    private EditText emailtxt, passtxt;
-    private Button signupbtn, loginbtn;
+    private EditText loginEmail, loginPassword;
 
-    /*
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginEmail = findViewById(R.id.emailLogin);
+        loginPassword = findViewById(R.id.passwordLogin);
+
+        Button btnLogin = findViewById(R.id.loginButton);
+        Button btnRegistration = findViewById(R.id.registerButton);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, Registration.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
 
     }
-    */
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     public void loginUser() {
-        String email = emailtxt.getText().toString().trim();
-        String password = passtxt.getText().toString().trim();
+        String email = loginEmail.getText().toString().trim();
+        String password = loginPassword.getText().toString().trim();
 
         if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "No empty field", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "All fields should be filled!", Toast.LENGTH_SHORT).show();
             return;
         }
+        mAuth.signInWithEmailAndPassword(email, password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this,
+                                    "Login successfully! ", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Failed login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
